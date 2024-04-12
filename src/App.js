91379f45f11013d7398d66d61814fb6f269bc18e
ziyang-666 <song.ziya@northeastern.edu>
2024-04-11 22:25:00 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header'
+import ChatArea from './components/ChatArea';
+import InputArea from './components/InputArea';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [messages, setMessages] = useState([]);
+
+    const handleSend = (text) => {
+        setMessages([...messages, { text, isUser: true }]);
+        // Simulate a response
+        setTimeout(() => {
+            setMessages(msgs => [...msgs, { text: "Echo: " + text, isUser: false }]);
+        }, 500);
+    };
+
+    return (
+        <div className="App">
+            <Header />
+            <ChatArea messages={messages} />
+            <InputArea onSend={handleSend} />
+        </div>
+    );
 }
 
 export default App;
